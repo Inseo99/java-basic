6bc2a2d243aa0649fadbad6d0ae794fc2c0155e1
inseo99 <jjj990626@naver.com>
2024-08-28 16:44:50 +0900

    Day4 file add

--- src/main/java/Day4/vending/VendingMachine.java
+++ src/main/java/Day4/vending/VendingMachine.java
@@ -4,9 +4,27 @@ import java.util.Scanner;
 
 public class VendingMachine {
 
-    public void  {
+    int total = 0;
+    String[] drink = {"콜라", "사이다", "커피"};
+    int[] price = {1000, 1200, 800};
 
+    public void inputMoney(int money) {
+        total = total + money;
+    }
 
+    public String selectBeverage(int choice) {
+        total = total- price[choice];
+        return drink[choice];
+    }
+
+    public int getRemainder() {
+        return total;
+    }
+
+    public void printBeverages() {
+        for (int i = 0; i < drink.length; i++) {
+            System.out.println(i + ". " + drink[i] + " : " + price[i]  + "원");
+        }
     }
 
 }
