f56476931ba821ff6f0ba4cc2e39d17cc65c2736
inseo99 <jjj990626@naver.com>
2024-09-02 16:39:35 +0900

    day7 file add

--- src/main/java/day7/polymopism/Exam.java
+++ src/main/java/day7/polymopism/Exam.java
@@ -18,30 +18,41 @@ class Warrior {
     Weapon weapon;
 
     public void attack() {
-        weapon.weapon(name);
+        weapon.attack(name);
     }
-    void introduce() {
+    public void introduce() {
         System.out.println("안녕하세요. 저는 " + age + "살 " + name + " 입니다.");
     }
+    public void useSkill() {
+        weapon.useSkill(name);
+    }
+
 
 }
 
 class Weapon {
-    public void weapon(String name) {
+    public void attack(String name) {
         System.out.println("무기를 들고 싸운다.");
+    }
+    public void useSkill(String name) {
+        System.out.println("스킬을 사용한다.");
     }
 }
 
 class Bow extends Weapon {
-    String name;
-    public void weapon(String name) {
+    public void attack(String name) {
         System.out.println(name + "이/가 활로 공격합니다.");
+    }
+    public void useSkill(String name) {
+        System.out.println(name + "이/가 불화살 스킬을 사용합니다. 1.5배(15)의 피해를 입힙니다.");
     }
 }
 
 class Sword extends Weapon {
-    String name;
-    public void weapon(String name) {
+    public void attack(String name) {
         System.out.println(name + "이/가 칼로 공격합니다.");
+    }
+    public void useSkill(String name) {
+        System.out.println(name + "이/가 연속 베기 스킬을 사용합니다. 2배(30)의 피해를 입힙니다.");
     }
 }
--- src/main/java/day7/polymopism/Exam2.java
+++ src/main/java/day7/polymopism/Exam2.java
@@ -5,19 +5,23 @@ public class Exam2 {
 
         Warrior warrior = new Warrior();
 
-        String name = "홍길동";
+        String name = "이순신";
         warrior.name = name;
-        warrior.age = 20;
+        warrior.age = 24;
         warrior.introduce();
         // 안녕하세요 저는 20살 홍길동입니다.
 
         warrior.weapon = new Bow();
         warrior.attack();
         // 출력 : 홍길동이/가 활로 공격합니다.
+        warrior.useSkill();
+        // 출력 : 홍길동이/가 불화살 스킬을 사용합니다. 1.5배(15)의 피해를 입힙니다.
 
         warrior.weapon = new Sword();
         warrior.attack();
         // 출력 : 홍길동이/가 칼로 공격합니다.
+        warrior.useSkill();
+        // 출력 : 홍길동이/가 연속 베기 스킬을 사용합니다. 2배(30)의 피해를 입힙니다.
     }
 }
 
